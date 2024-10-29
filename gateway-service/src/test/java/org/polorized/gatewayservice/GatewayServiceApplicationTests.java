2cfbc1208704cacab1f111806d390cdc83a951ba
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-10-29 15:44:17 +0000

    session:management

---
+++ gateway-service/src/test/java/org/polorized/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.polorized.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
